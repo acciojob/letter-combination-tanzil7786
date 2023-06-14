fbc3e943c76b4c7c948537775353919adcc83129
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-06-14 21:39:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-function letterCombinations(input_digit) {
-  //Complete the function
+function letterCombinations(digits) {
+  if (digits.length === 0) {
+    return [];
+  }
+
+  const digitToLetters = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz',
+  };
+
+  const result = [];
+
+  function backtrack(combination, nextDigits) {
+    if (nextDigits.length === 0) {
+      result.push(combination);
+      return;
+    }
+
+    const currentDigit = nextDigits[0];
+    const letters = digitToLetters[currentDigit];
+
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      backtrack(combination + letter, nextDigits.slice(1));
+    }
+  }
+
+  backtrack('', digits);
+
+  return result;
 }
 
 module.exports = letterCombinations;
